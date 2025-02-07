25773c5f50106192829d22bfa7a382098f51d106
Jo Allam <joallam2008@gmail.com>
2025-02-07 20:48:43 +0200

    Trying to do anything really

--- asteroidFetcher.js
+++ asteroidFetcher.js
@@ -1,12 +1,9 @@
-
-
 fetch("https://api.nasa.gov/neo/rest/v1/feed?start_date=START_DATE&end_date=END_DATE&api_key=API_KEY")
 .then(function(data) {
-    console.log(data);
+    console.log(data.headers.date);
 })
 
 let asteroidReceived = [];
 
-for(i = 0; i < 10; i++) {
-    
-}
+
+
